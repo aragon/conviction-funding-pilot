eb211e529e58af932733cbced7eec68ba826945e
Enrique Ortiz <hi@enriqueortiz.dev>
2020-08-25 09:52:48 -0400

    Leave a note about explicit mainnet-only support

--- src/environment.js
+++ src/environment.js
@@ -15,6 +15,10 @@ const ENV_VARS = {
   WALLETCONNECT_RPC_URL() {
     return (
       process.env.REACT_APP_WALLETCONNECT_RPC_URL ||
+      // We're not dynamically setting the network due to the
+      // web3-react WalletConnect connector being broken on any chain
+      // > 1.
+      // https://github.com/NoahZinsmeister/web3-react/blob/v6/packages/walletconnect-connector/src/index.ts#L31
       'https://mainnet.eth.aragon.network/'
     )
   },
