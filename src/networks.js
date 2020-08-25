cf92fc643c9ff044093b3b62b33c48f5db5f7c7f
Enrique Ortiz <hi@enriqueortiz.dev>
2020-08-24 23:35:32 -0400

    Add quicc accrual org

--- src/networks.js
+++ src/networks.js
@@ -15,7 +15,7 @@ const networks = {
     ensRegistry: '0x98df287b6c145399aaa709692c8d308357bc085d',
     defaultEthNode: 'wss://rinkeby.eth.aragon.network/ws',
     name: 'Rinkeby',
-    orgAddress: '0xccee96a88fe7d166601b0af6db3fbbbb5b4fa8b8',
+    orgAddress: '0xfce879d3ce15609d806691b7390bcee685159774',
     type: 'rinkeby',
   },
   xdai: {
