34e7e50dd91430f70f786942ef1d0c23aeffe94d
Enrique Ortiz <hi@enriqueortiz.dev>
2020-08-26 12:48:58 -0400

    Scroll to top on reload

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useCallback, useEffect } from 'react'
 import { Main } from '@aragon/ui'
 import { useAppState } from '../providers/AppState'
 
 export default function MainWrapper({ children }) {
   const { appearance } = useAppState()
+
+  const scrollTop = useCallback(() => window.scrollTo(0, 0), [])
+
+  useEffect(() => {
+    window.addEventListener('beforeunload', scrollTop)
+
+    return () => window.removeEventListener('beforeunload', scrollTop)
+  }, [scrollTop])
 
   return (
     <Main assetsUrl="/aragon-ui/" layout={false} theme={appearance}>
